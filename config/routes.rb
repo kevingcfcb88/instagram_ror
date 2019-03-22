ff2711df464c7364945062cb758570e5e391cded
Kevin <kevingcfcb88@gmail.com>
2019-03-21 21:22:54 -0600

    Delete post feature added

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to:'home#index'
   resources :users, only: [:show, :edit, :update]
-  resources :posts, only: [:new, :create]
+  resources :posts, only: [:new, :create, :show, :destroy]
 end
